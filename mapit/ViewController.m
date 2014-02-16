e45b265ec8b165d4270d0b33a7bb1ed961d62ef7
magskyic <zzhan@buffalo.edu>
2014-02-16 09:39:41 -0500

    map kit using
    first try Xcode 5 using map kit

--- mapit/ViewController.m
+++ mapit/ViewController.m
@@ -12,12 +12,38 @@
 
 @end
 
+//my location
+#define my_LATITUDE
+#define my_LONGTITUDE
+
+//SPAN
+#define THE_SPAN 0.01f;
+
+
 @implementation ViewController
+@synthesize myMapview;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	// reate the region
+    MKCoordinateRegion myRegion;
+    
+    //center
+    CLLocationCoordinate2D center;
+    center.latitude = my_LATITUDE 42.992883;
+    center.longitude = my_LONGTITUDE -78.784673;
+    
+    //span
+    MKCoordinateSpan span;
+    span.latitudeDelta = THE_SPAN;
+    span.longitudeDelta = THE_SPAN;
+    
+    myRegion.center =center;
+    myRegion.span = span;
+    
+    //set our mapview
+    [myMapview setRegion:myRegion animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
